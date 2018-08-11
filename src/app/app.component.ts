743c1a8db9f4ef7ba7161760a777636171558700
gaelleF <gaelle.fougere@outlook.fr>
2018-08-11 21:33:30 +0200

    display post from array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'this amazing blog';
+  posts = [
+    {title: 'mon premier post',
+     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam euismod justo vitae vehicula. Aliquam varius ligula vitae tempor mattis. Etiam at ex nulla. ',
+     loveIts: 0,
+    created_at: new Date()},
+    {title: 'mon deuxieme post',
+     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam euismod justo vitae vehicula. Aliquam varius ligula vitae tempor mattis. Etiam at ex nulla. ',
+     loveIts: -3,
+    created_at: new Date()},
+    {title: 'Encore un post',
+     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam euismod justo vitae vehicula. Aliquam varius ligula vitae tempor mattis. Etiam at ex nulla. ',
+     loveIts: 10,
+    created_at: new Date()}
+  ]
 }
